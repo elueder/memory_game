79756f4cc6c6094fc3f2cf0b2f3cf57ddf30304d
elueder <lizzie.lueder@gmail.com>
2018-05-22 15:24:17 -0400

    fixed bug cards not appearing

--- js/main.js
+++ js/main.js
@@ -32,32 +32,28 @@ function checkForMatch(){
 			alert("Sorry, try again.");
 		}
 	}
-}
+};
 
 
 function flipCard(){
 	var cardId = this.getAttribute('data-id');
-	this.setAttribute('src', cards[cardId].cardImage);
-	cardsInPlay.push(cards[cardId].rank);
-
 	console.log("User flipped " + cards[cardId].rank);
-
+	cardsInPlay.push(cards[cardId].rank);
+	this.setAttribute('src', cards[cardId].cardImage);
+	checkForMatch();
 	console.log(cards[cardId].cardImage);
 	console.log(cards[cardId].suit);
-
-	/*if (cardsInPlay.length === 2) {
-		checkForMatch();
-	}*/
-}
+};
 
 function createBoard(){
 	for (var i = 0; i < cards.length; i++) {
 	 var newListItem = document.createElement('img');
-	 cardElement.setAttribute('src', "images/back.png");
-	 cardElement.setAttribute('data-id', i);
-	 cardElement.addEventListener('click', flipCard);
-	 document.getElementById('game-board').appendChild(newListItem);
+	 newListItem.setAttribute('src', 'css/images/back.png');
+	 newListItem.setAttribute('data-id', i);
+	 newListItem.addEventListener('click', flipCard);
+	 var gameBoard = document.getElementById('game-board');
+	 gameBoard.appendChild(newListItem);
 	}
-}
+};
 
 createBoard();
